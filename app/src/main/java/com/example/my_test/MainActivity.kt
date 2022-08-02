d2965c5d408ded115d292d30b3744354f0203289
eric <eric1.chou@tpisoftware.com>
2022-08-02 15:53:07 +0800

    clean old interview code

--- app/src/main/java/com/example/my_test/MainActivity.kt
+++ app/src/main/java/com/example/my_test/MainActivity.kt
@@ -18,38 +18,17 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: ProfileViewModel by viewModels()
-    private val adapter = ProfileDetailRecyclerViewAdapter(listOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setRecyclerView()
-        getApiData()
-        dataObserve()
         setContentView(binding.root)
-    }
-
-    private fun dataObserve() {
-        viewModel.profileList.observe(this) {
-            adapter.updateList(it)
-        }
-    }
-
-    private fun getApiData() {
-        viewModel.getProfileList()
     }
 
 
     private fun setRecyclerView() {
         binding.recyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        binding.recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                this,
-                DividerItemDecoration.VERTICAL
-            )
-        )
-        binding.recyclerView.adapter = adapter
     }
 }
